a31dcffd46973a7fb3821606c81308746f001a86
Jonathan Hess <info@movingwater.ch>
2016-06-26 21:07:14 +0200

    supportstatus -> indiegogo

--- site/snippets/sections/supportstatus.php
+++ site/snippets/sections/supportstatus.php
@@ -1,53 +1,53 @@
 <?php if(!$section->hide()->isTrue()): ?>
 <script type="text/javascript" language="javascript">
    window.addEventListener('load', function (){
-     var totalAmount = 75000;
-     var raisedAmount = 60076;
-     var contributors = 25;
-     var percentage = Math.round(100 * raisedAmount / totalAmount);
-     document.getElementById('counter').innerHTML = '$ '+ numberWithCommas(raisedAmount);
-     document.getElementById('percentbar').style.width = percentage + '%';
-     document.getElementById('percenttext').innerHTML = '<strong>' + percentage + '%</strong>';
-     document.getElementById('amountend').innerHTML = '$ '+ numberWithCommas(totalAmount);
-     if(percentage < 25) {
-       $('#counter').addClass('outside');
-     } else {
-       $('#counter').addClass('inside');
-     };
-     if(percentage < 45) {
-       $('#counter').addClass('outside-m');
-       document.getElementById('counter').style.left = percentage + '%';
-     };
-     // Viewportchecker
-     $('.start').viewportChecker({
-       classToRemove: 'zero',
-       offset: 100
-     });
-    // $.getJSON("https://api.indiegogo.com/1/campaigns/14184598.json?api_token=a3a779b95c1f3c4e6ae3c031ac2da1a864969516007b1b5ecd1799caeeb86b9f", function(gogodata) {
-    //   var $totalAmount = numberWithCommas(gogodata.response.goal);
-    //   var $raisedAmount = numberWithCommas(gogodata.response.collected_funds);
-    //   var $contributors = numberWithCommas(gogodata.response.contributions_count);
-    //   var percentage = Math.round(100 * raisedAmount / totalAmount);
-    //
-    //   document.getElementById('counter').innerHTML = '$ '+ numberWithCommas(raisedAmount);
-    //   document.getElementById('percentbar').style.width = percentage + '%';
-    //   document.getElementById('percenttext').innerHTML = '<strong>' + percentage + '%</strong>';
-    //   document.getElementById('amountend').innerHTML = '$ '+ numberWithCommas(totalAmount);
-    //   if(percentage < 25) {
-    //     $('#counter').addClass('outside');
-    //   } else {
-    //     $('#counter').addClass('inside');
-    //   };
-    //   if(percentage < 45) {
-    //     $('#counter').addClass('outside-m');
-    //     document.getElementById('counter').style.left = percentage + '%';
-    //   };
-    //   // Viewportchecker
-    //   $('.start').viewportChecker({
-    //     classToRemove: 'zero',
-    //     offset: 100
-    //   });
-    // });
+    //  var totalAmount = 75000;
+    //  var raisedAmount = 60076;
+    //  var contributors = 25;
+    //  var percentage = Math.round(100 * raisedAmount / totalAmount);
+    //  document.getElementById('counter').innerHTML = '$ '+ numberWithCommas(raisedAmount);
+    //  document.getElementById('percentbar').style.width = percentage + '%';
+    //  document.getElementById('percenttext').innerHTML = '<strong>' + percentage + '%</strong>';
+    //  document.getElementById('amountend').innerHTML = '$ '+ numberWithCommas(totalAmount);
+    //  if(percentage < 25) {
+    //    $('#counter').addClass('outside');
+    //  } else {
+    //    $('#counter').addClass('inside');
+    //  };
+    //  if(percentage < 45) {
+    //    $('#counter').addClass('outside-m');
+    //    document.getElementById('counter').style.left = percentage + '%';
+    //  };
+    //  // Viewportchecker
+    //  $('.start').viewportChecker({
+    //    classToRemove: 'zero',
+    //    offset: 100
+    //  });
+    $.getJSON("https://api.indiegogo.com/1/campaigns/14184598.json?api_token=a3a779b95c1f3c4e6ae3c031ac2da1a864969516007b1b5ecd1799caeeb86b9f", function(gogodata) {
+      var $totalAmount = numberWithCommas(gogodata.response.goal);
+      var $raisedAmount = numberWithCommas(gogodata.response.collected_funds);
+      var $contributors = numberWithCommas(gogodata.response.contributions_count);
+      var percentage = Math.round(100 * raisedAmount / totalAmount);
+
+      document.getElementById('counter').innerHTML = '$ '+ numberWithCommas(raisedAmount);
+      document.getElementById('percentbar').style.width = percentage + '%';
+      document.getElementById('percenttext').innerHTML = '<strong>' + percentage + '%</strong>';
+      document.getElementById('amountend').innerHTML = '$ '+ numberWithCommas(totalAmount);
+      if(percentage < 25) {
+        $('#counter').addClass('outside');
+      } else {
+        $('#counter').addClass('inside');
+      };
+      if(percentage < 45) {
+        $('#counter').addClass('outside-m');
+        document.getElementById('counter').style.left = percentage + '%';
+      };
+      // Viewportchecker
+      $('.start').viewportChecker({
+        classToRemove: 'zero',
+        offset: 100
+      });
+    });
   });
   function numberWithCommas(x) {
     <?php if($site->language()->code() == 'en'): ?>
